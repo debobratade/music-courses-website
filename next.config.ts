bdb503f70725c04042aea8e448a19e13b2ac376f
debobratade <debobratadeyd@gmail.com>
2024-11-03 18:47:57 +0530

    first push of music courses website

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['cherryvalleymusic.com','mbmusicacademy.org','cdn.shopify.com','cdn.prod.website-files.com', 'stringjoy.s3.amazonaws.com','media.licdn.com','e7.pngegg.com','img.freepik.com','img.freepik.com','images.unsplash.com','encrypted-tbn0.gstatic.com','plus.unsplash.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
---
+++ src/components/WhyChooseUs.tsx
@@ -0,0 +1,81 @@
+"use client";
+import React from "react";
+import { StickyScroll } from "./ui/sticky-scroll-reveal";
+import Image from "next/image";
+
+const content = [
+  {
+    title: "Discover Your Sound with Us: A Personal Journey in Music Mastery",
+    description:
+      "Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
+    content: (
+      <div className="h-full w-full  flex items-center justify-center text-white">
+        <Image
+          src="https://e7.pngegg.com/pngimages/236/622/png-clipart-ai-generated-guitar-colorful-rainbow-e-guitar-instrument-musical.png"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
+      </div>
+    ),
+  },
+  {
+    title: "Live Feedback & Engagement",
+    description:
+      "Immerse yourself in an interactive learning experience where feedback is immediate, just like real-time changes in a collaborative project. This approach enhances your understanding and mastery of music concepts and performance techniques.",
+    content: (
+      <div className="h-full w-full  flex items-center justify-center text-white">
+        <Image
+          src="https://media.licdn.com/dms/image/D5612AQFlvydS5YfOdA/article-cover_image-shrink_720_1280/0/1691767316831?e=2147483647&v=beta&t=tDRHuqSm09RuTt2-HYFMUOSHB4OdYSN6LlrDlh2fgjw"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
+      </div>
+    ),
+  },
+  {
+    title: "Cutting-Edge Curriculum",
+    description:
+      "Our curriculum is continuously updated to include the latest music education trends and technologies, ensuring you’re always learning with the most current and effective methods. Say goodbye to outdated materials and welcome an education that evolves with the industry.",
+    content: (
+      <div className="h-full w-full  flex items-center justify-center text-white">
+        <Image
+          src="https://img.freepik.com/premium-photo/musical-instrument-arrangement-room-setting_952286-23175.jpg"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
+      </div>
+    ),
+  },
+  {
+    title: "Limitless Learning Opportunities",
+    description:
+      "With our expansive resource library and dynamic course offerings, you’ll never find yourself without something new to explore. Our platform provides continuous opportunities for growth, ensuring your musical skills are always advancing.",
+    content: (
+      <div className="h-full w-full  flex items-center justify-center text-white">
+        <Image
+          src="https://img.freepik.com/premium-photo/painting-woman-playing-piano_1072138-208228.jpg"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
+      </div>
+    ),
+  },
+];
+
+function WhyChooseUs() {
+  return (
+    <div>
+      <StickyScroll content={content} />
+    </div>
+  );
+}
+
+export default WhyChooseUs;
